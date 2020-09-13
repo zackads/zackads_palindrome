d4ab1348a02a246d88d1be8b4fce677c2a86de0b
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-09-13 14:05:01 +0100

    Create README.md

--- test/zackads_palindrome_test.rb
+++ test/zackads_palindrome_test.rb
@@ -1,11 +1,11 @@
-require "test_helper"
+require 'test_helper'
 
 class ZackadsPalindromeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ZackadsPalindrome::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_non_palindrome
+    assert !'apple'.palindrome?
   end
 end
