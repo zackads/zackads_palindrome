a46f6441260343496902c237ccca4b61a32081f0
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-09-13 14:22:42 +0100

    Test mixed case palindrome

--- test/zackads_palindrome_test.rb
+++ test/zackads_palindrome_test.rb
@@ -8,4 +8,16 @@ class ZackadsPalindromeTest < Minitest::Test
   def test_non_palindrome
     assert !'apple'.palindrome?
   end
+
+  def test_literal_palindrome
+    assert 'racecar'.palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert 'Racecar'.palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    skip
+  end
 end
